e852994c00095751daf76790d40fd0433325257d
shankarprabhu-s <prabhushankar340@gmail.com>
2024-01-22 23:35:00 +0530

    Products Added

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import './home.scss';
+import ProductCard from "../components/productCard";
+import './product.scss'
 
 function Product(){
     return(
-        <div className="home_container">
-            welcome to Products section
+        <>
+
+        <div className="ExtraMargin"> {/*to provide the margin at the row of  first row of product view*/}
+        <ProductCard/>
         </div>
+        <ProductCard/>
+           
+        </>
     )
 }
 
