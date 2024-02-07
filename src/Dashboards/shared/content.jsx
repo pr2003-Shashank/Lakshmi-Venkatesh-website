7c1443f48ce20d38e5b1681bbe6b9eeeaf0cbe89
Shashank P R <138097341+pr2003-Shashank@users.noreply.github.com>
2024-02-08 00:07:16 +0530

    Dashboard rectification

--- src/Dashboards/shared/content.jsx
+++ src/Dashboards/shared/content.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-import './content.scss';
+import Overview from "../components/overview"; 
+import ProductManagement from '../components/productManagement';
+import UserManagement from '../components/userManagement';
+import OrderManagement from '../components/orderManagement';
+import Account from '../components/account';
+import './content.scss'
 
-function Content(){
-    return(
-        <div className="content_container">
-            
-        </div>
-    )
+function Content({selectedPage}) {
+ 
+  // Conditionally render components based on the selected page
+
+  switch (selectedPage) {
+    case '':
+      return <Overview />;
+    case 'products':
+      return <ProductManagement />;
+    case 'users':
+      return <UserManagement />;
+    case 'orders':
+      return <OrderManagement />;
+      case 'account':
+        return <Account/>;
+    default:
+      return null;
+  }
 }
 
 export default Content;
